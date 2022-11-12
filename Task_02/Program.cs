730347e01df614aa5b46ea36f75f15d07fbdfde1
Vladimir <zarguitar@list.ru>
2022-11-12 23:37:28 +0300

    Если что-то упустили

---
+++ Task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} большее, а {number2} - меньшее ");
+}
+else 
+{
+    Console.WriteLine($"Число {number2} большее, а {number1} меньшее ");
+}
