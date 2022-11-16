51ed36045f2887afed1b884c56cc97480b55b68d
Vladimir <zarguitar@list.ru>
2022-11-16 23:17:08 +0300

    commitem all

---
+++ task_04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)max=num2;
+if (num3 > max)max=num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ task_06/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 ==0) Console.WriteLine($"число {num} является четным ");
+
+else Console.WriteLine($"число {num} не является четным "); 
