172d4636932d81d3da6a218175a6d03931789361
Vladimir <zarguitar@list.ru>
2022-11-17 13:17:19 +0300

    task finally complete

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -5,7 +5,7 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 
 if (number1 > number2) Console.WriteLine($"Число {number1} большее, а {number2} - меньшее ");
 
-if  (number1 < number2) Console.WriteLine($"Число {number2} большее, а {number1} меньшее ");
+else if (number1 < number2) Console.WriteLine($"Число {number2} большее, а {number1} меньшее ");
 
 else 
 Console.WriteLine("Числа равны ");
