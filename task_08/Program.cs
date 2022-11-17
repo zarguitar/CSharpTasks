97dbbc32ff19fba80428ac46cef3746011f4eecb
Vladimir <zarguitar@list.ru>
2022-11-17 13:06:02 +0300

    task complete

---
+++ task_08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= num)
+{
+    if (count % 2 == 0)
+    Console.Write($"{count} ");
+ 
+ count ++;
+ }
+ 
