29cbb106c7f61a2eb4f9abbdb3c4f4c7e850dd4c
Vladimir <zarguitar@list.ru>
2022-12-03 22:13:29 +0300

    Task complete

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumCombo(int N)
+{
+    int sum = 0;
+     while (N > 0)
+     {
+       sum = sum + N % 10;
+       N = N / 10;
+     }
+    return sum;
+}
+int sumcombo = SumCombo(num);
+Console.Write($"{num} --> {sumcombo}");
