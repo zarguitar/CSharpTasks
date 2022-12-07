44b5bad4a7eb37bf5a5f679732d9f48034ea683e
Vladimir <zarguitar@list.ru>
2022-12-07 23:05:41 +0300

    Task complete

---
+++ Task_36/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(4, -100, 100);
+PrintArray(array);
+
+int CatchOddIndex(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum += arr[i];
+    }
+    return sum;
+
+
+}
+
+
+int catchoddindex = CatchOddIndex(array);
+Console.WriteLine($" -> {catchoddindex}");
