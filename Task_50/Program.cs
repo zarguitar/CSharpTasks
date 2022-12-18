d8b3d8fdd0ed2189aa8498c0be31b852199dffd5
Vladimir <zarguitar@list.ru>
2022-12-18 23:19:25 +0300

    Version correct

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -6,39 +6,39 @@
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
 
-    Console.WriteLine("Задайте позицию i ");
-    int row = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Задайте позицию j ");
-    int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте позицию i ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте позицию j ");
+int col = Convert.ToInt32(Console.ReadLine());
 
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-int[,] matrix = new int[rows, columns];
-Random rnd = new Random();
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
 
-for (int i = 0; i < matrix.GetLength(0); i++) 
-{
-for (int j = 0; j < matrix.GetLength(1); j++) 
-{
-matrix[i, j] = rnd.Next(min, max + 1); 
-}
-}
-return matrix;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
 }
 
 void PrintMatrix(int[,] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-Console.Write("[");
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
-else Console.Write($"{matrix[i, j], 4} ");
-}
-Console.WriteLine("]");
-}
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
 }
 
 void ShowElementMatrix(int[,] matrix)
